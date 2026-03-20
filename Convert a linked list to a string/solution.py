5634801125a58fa39bded596009636c260c83486
Zavada Sofiia <zavada.sofia@gmail.com>
2026-03-20 12:20:20 +0200

    Update solution.py

--- Convert a linked list to a string/solution.py
+++ Convert a linked list to a string/solution.py
@@ -1,1 +1,13 @@
-
+"""
+Convert a linked list to a string
+"""
+def stringify(node):
+    values = []
+    current = node
+    while current:
+        values.append(str(current.data))
+        current = current.next
+        
+    values.append(str(None))
+    
+    return " -> ".join(values)

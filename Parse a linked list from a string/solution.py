ae04179e018c6b97fb1e3db6224406308a81e133
Zavada Sofiia <zavada.sofia@gmail.com>
2026-03-25 12:26:50 +0200

    Update solution.py

--- Parse a linked list from a string/solution.py
+++ Parse a linked list from a string/solution.py
@@ -1,1 +1,12 @@
+from preloaded import Node
 
+def linked_list_from_string(list_repr: str) -> Node | None:
+    
+    values = list_repr.split(" -> ")
+    
+    head = None
+    
+    for value in reversed(values[:-1]):
+        head = Node(int(value), head)
+    
+    return head

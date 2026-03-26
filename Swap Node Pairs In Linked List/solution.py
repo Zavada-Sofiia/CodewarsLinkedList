09b7b7c5b1e181c1cfc24ba29ff8a3dd4d8eb672
Zavada Sofiia <zavada.sofia@gmail.com>
2026-03-26 19:22:24 +0200

    Update solution.py

--- Swap Node Pairs In Linked List/solution.py
+++ Swap Node Pairs In Linked List/solution.py
@@ -1,1 +1,17 @@
+from preloaded import Node
 
+def swap_pairs(head):
+
+    if not head:
+        return head
+    if not head.next:
+        return head
+
+    first = head
+    second = head.next
+
+    first.next = swap_pairs(second.next)
+
+    second.next = first
+
+    return second
